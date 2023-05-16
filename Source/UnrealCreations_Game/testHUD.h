099cdfe2e777d8b86cb946ac3394c20c233b0faa
Ashley DeMott <ashley.demott28@gmail.com>
2023-05-16 14:45:45 -0600

    Testing UI

---
+++ Source/UnrealCreations_Game/testHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "testHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALCREATIONS_GAME_API AtestHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
